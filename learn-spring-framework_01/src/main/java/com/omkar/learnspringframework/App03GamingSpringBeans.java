5158ce2a866f055b9f0a28713f5ff824030d2b7b
Omkar0017 <omkarp0017@gmail.com>
2024-02-25 16:23:41 +0000

    Update Files
    Removed unwanted files and added configuration in launcher class itself

--- learn-spring-framework_01/src/main/java/com/omkar/learnspringframework/App03GamingSpringBeans.java
+++ learn-spring-framework_01/src/main/java/com/omkar/learnspringframework/App03GamingSpringBeans.java
@@ -2,7 +2,29 @@ package com.omkar.learnspringframework;
 
 import com.omkar.learnspringframework.game.GameConsole;
 import com.omkar.learnspringframework.game.GameRunner;
+import com.omkar.learnspringframework.game.PacMan;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+class GamingConfiguration {
+
+  @Bean
+  public GameConsole game(){
+    var game = new PacMan();
+    return game;
+  }
+
+  @Bean
+  public GameRunner gameRunner(GameConsole game){
+    var gamerunner = new GameRunner(game);
+    return gamerunner;
+  }
+}
+
+
 
 public class App03GamingSpringBeans  {
 
