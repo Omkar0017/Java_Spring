d5e1473a10138e783d7e94bd891fa1137cd31d50
Omkar0017 <omkarp0017@gmail.com>
2024-06-17 14:51:41 +0100

    Customising Json

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.in28minutes.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 import java.time.LocalDate;
@@ -10,6 +11,7 @@ public class User {
 
   private Integer id;
   @Size(min = 2, message = "Name should have atleast 2 characters")
+  @JsonProperty("UserName")
   private String name;
   @Past(message = "Birthdate cannot be future date")
   private LocalDate birthDate;
