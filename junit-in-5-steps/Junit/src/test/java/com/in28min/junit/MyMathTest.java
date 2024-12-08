4e16d4fd5dfb42a2d3fab5b512e7f269b5e85d57
omkar0017 <omkarp0017@gmail.com>
2024-12-08 22:13:36 +0000

    Initial commit for Unit Test

---
+++ junit-in-5-steps/Junit/src/test/java/com/in28min/junit/MyMathTest.java
@@ -0,0 +1,12 @@
+package com.in28min.junit;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.fail;
+
+public class MyMathTest {
+
+    @Test
+    public void calculateSumTest(){
+    }
+}
