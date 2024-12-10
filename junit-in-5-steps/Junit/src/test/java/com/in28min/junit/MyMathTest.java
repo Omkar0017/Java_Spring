1e161bb0b7b51fe4c8071b7f1730257aac830066
omkar0017 <omkarp0017@gmail.com>
2024-12-10 21:42:10 +0000

    "Making Unit tests compact"

--- junit-in-5-steps/Junit/src/test/java/com/in28min/junit/MyMathTest.java
+++ junit-in-5-steps/Junit/src/test/java/com/in28min/junit/MyMathTest.java
@@ -6,27 +6,19 @@ import static junit.framework.TestCase.*;
 
 public class MyMathTest {
 
+    private  MyMath myMath = new MyMath();
 
     @Test
     public void calculateSumTest(){
-        MyMath myMath = new MyMath();
-        int[] number1 = {1,3,5,7};
-        int result = myMath.calculateSum(number1);
-        int expected = 16;
-        assertEquals(expected,result);
 
-        int[] number2 = {1,2,3};
-        assertNotSame(10,myMath.calculateSum(number2));
+        assertEquals(16,myMath.calculateSum(new int[]{1,3,5,7}));
+        assertNotSame(10,myMath.calculateSum(new int[]{1,2,3}));
     }
 
     @Test
     public void calculateSum_emptyArray(){
-        MyMath myMath = new MyMath();
-        int[] number = {} ;
-        int result = myMath.calculateSum(number);
-        int expexted = 0;
 
-        assertEquals(expexted,result);
+        assertEquals(0,myMath.calculateSum(new int[]{}));
 
     }
 
