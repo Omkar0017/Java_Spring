b72297610dc78ca4f8f00dcd2c2e087ebbe5b2e3
omkar0017 <omkarp0017@gmail.com>
2025-01-15 15:22:18 +0000

    Using Lamba expression

--- Functional_programing_java/src/main/java/com/in28min/programing/FP01Functional.java
+++ Functional_programing_java/src/main/java/com/in28min/programing/FP01Functional.java
@@ -2,17 +2,36 @@ package com.in28min.programing;
 
 import java.util.List;
 
+import static com.in28min.programing.FP01Structure.BreakPattern;
+
 public class FP01Functional {
     public static void main(String[] args) {
-        printAllNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+        List<Integer> numbers = List.of(12, 9, 13, 4, 6, 2, 4, 12, 15);
+        printAllNumbersInListFunctional(numbers);
+        System.out.println(BreakPattern);
+        printEvenNumbersInListFunctional(numbers);
+
 
     }
 
-    private static void print(Integer integer){
-        System.out.println(integer);
+    private static boolean isEven(int number){
+        return number%2 ==0;
     }
+    private static void printEvenNumbersInListFunctional(List<Integer> integers) {
+
+//        integers.stream().filter(FP01Functional::isEven)
+//                .forEach(System.out::println);
+        integers.stream().filter(integer -> integer%2==0)
+                .forEach(System.out::println);
+    }
+
+//    private static void print(Integer integer){
+//        System.out.println(integer);
+//    }
 
     private static void printAllNumbersInListFunctional(List<Integer> integers) {
-        integers.forEach(FP01Functional::print); // :: is method reference
+//        integers.forEach(FP01Functional::print); // :: is method reference
+        integers.forEach(System.out::println);
     }
+
 }
--- Functional_programing_java/src/main/java/com/in28min/programing/FP01Structure.java
+++ Functional_programing_java/src/main/java/com/in28min/programing/FP01Structure.java
@@ -4,8 +4,20 @@ import java.util.List;
 
 public class FP01Structure {
 
+    public static final String BreakPattern = "************************";
+
     public static void main(String[] args) {
         printAllNumbersInListStructure(List.of(12,9,13,4,6,2,4,12,15));
+        System.out.println(BreakPattern);
+        printEvenNumbersInListStructure(List.of(12,9,13,4,6,2,4,12,15));
+    }
+
+    private static void printEvenNumbersInListStructure(List<Integer> integers) {
+        for(int i:integers){
+            if(i%2 ==0){
+                System.out.println(i);
+            }
+        }
     }
 
     private static void printAllNumbersInListStructure(List<Integer> integers) {
